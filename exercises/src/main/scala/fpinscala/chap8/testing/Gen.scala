91941c731450dff13563a7fd59e338a09cc2f1d6
Benjamin Denom <ben.denom@gmail.com>
2020-08-23 21:28:45 +0200

    Reorganize files in chapters packages

---
+++ exercises/src/main/scala/fpinscala/chap8/testing/Gen.scala
@@ -0,0 +1,35 @@
+package fpinscala.chap8.testing
+
+import fpinscala.chap5.laziness.Stream
+import fpinscala.chap6.state._
+import fpinscala.chap7.parallelism._
+import fpinscala.chap7.parallelism.Par.Par
+import Gen._
+import Prop._
+import java.util.concurrent.{Executors,ExecutorService}
+
+/*
+The library developed in this chapter goes through several iterations. This file is just the
+shell, which you can fill in and modify while working through the chapter.
+*/
+
+trait Prop {
+}
+
+object Prop {
+  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+}
+
+object Gen {
+  def unit[A](a: => A): Gen[A] = ???
+}
+
+trait Gen[A] {
+  def map[A,B](f: A => B): Gen[B] = ???
+  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+}
+
+trait SGen[+A] {
+
+}
+
